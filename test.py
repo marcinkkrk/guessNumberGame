532f11dabc63f3f2b0592c9d39f0cc658fbc6459
marcinkkrk <65133294+marcinkkrk@users.noreply.github.com>
2022-11-23 21:33:48 +0100

    Add files via upload

--- test.py
+++ test.py
@@ -2,21 +2,23 @@ import random
 
 def game():
     randomNumber = random.randint(0,9)
+    numberOfChoices = 0
     guess = 10
     while randomNumber != guess:
+        numberOfChoices += 1
         guess = int(input("Guess number (between 0 and 9)\n"))
         if guess == randomNumber:
             print("BRAVO")
+            print(f"You needed {numberOfChoices} choices")
         elif guess >randomNumber:
             print("TOO HIGH")
         else:
             print("TOO LOW")
 
 x = "play"
-
 while x != "exit":
     game()
-    x = input("Type 'play' or 'exit'")
+    x = input("Type 'play' or 'exit'\n")
     if x == "play" or x == "exit":
         print("")
     else:
